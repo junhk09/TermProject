fab0e38b11f735bb4d1da19050841908a2bd2c3e
daye <dbk1102@naver.com>
2025-06-04 10:29:42 +0900

    fix: Enemy D 몬스터 애니메이션 적용

---
+++ Term/Assets/Scripts/Missile.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+
+public class Missile : MonoBehaviour
+{
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.right * 30 * Time.deltaTime);
+    }
+}
